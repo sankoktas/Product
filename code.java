96ea7626dcf5df67be5c93c68188ac4597af1deb
Fikri San Koktas <kokfik.21@robcol.k12.tr>
2021-06-18 22:50:31 +0300

    Update and rename README.md to code.java

---
+++ code.java
@@ -0,0 +1,26 @@
+public class Product
+{
+    private String name;
+    private double price;
+    
+    public Product (String productName, double productPrice)
+    {
+        name = productName;
+        price = productPrice;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void reducePrice(double byAmount)
+    {
+        price = price - byAmount;
+    }
+}
